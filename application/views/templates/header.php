687168bff1e7d373f331703b18de1f9a841e2006
Michael Brady <michaelbrady712@gmail.com>
2023-04-07 19:47:50 -0500

    Added mobile navbar

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -4,5 +4,26 @@
                 <link href="assets/css/main.css" rel="stylesheet">
         </head>
         <body>
+                <nav class="nav-section">
+                        <div id="mobnav">
+                                <input class="checkbox" type="checkbox"/>
+                                        <div class="hamburger-lines">
+                                                <span class="line line1"></span>
+                                                <span class="line line2"></span>
+                                                <span class="line line3"></span>
+                                        </div>  
+                                <div class="menu-items">
+                                        <a href="../index.php">Home</a>
+                                        <a href="../title.php">About</a>
+                                        <a href="../contact.php">News</a>
+                                        <a href="../contact.php">Create</a>
+                                </div>
+                        </div>
+                        <div id="normnav">
+                                <a href="../index.php">Home</a>
+                                <a href="../title.php">Title-Generator</a>
+                                <a href="../contact.php">Contact Me</a>
+                        </div>
+                </nav>
                 <div id="content-wrap">
                         <h1 class="page-header"><?= $title ?></h1>
