192f8fdb52c51498b21dc76d56294882e88196f3
Michael Brady <michaelbrady712@gmail.com>
2023-04-07 22:18:57 -0500

    Added styles to news page

--- application/views/news/index.php
+++ application/views/news/index.php
@@ -1,5 +1,7 @@
-<h3><?= $news_item['title'] ?></h3>
-        <div class="main">
-                <?= $news_item['text'] ?>
-        </div>
-<p><a href="<?= site_url('news/'.$news_item['slug']) ?>">View article</a></p>
+<div class="news-item">
+        <h3><?= $news_item['title'] ?></h3>
+                <div class="main">
+                        <?= $news_item['text'] ?>
+                </div>
+        <p><a href="<?= site_url('news/'.$news_item['slug']) ?>">View article</a></p>
+</div>
