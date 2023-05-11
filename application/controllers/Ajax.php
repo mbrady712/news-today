236cee0b6d69e34595c63f6f0b4fa13b0e2be15d
Michael Brady <michaelbrady712@gmail.com>
2023-05-11 16:17:04 -0500

    Added files for AJAX submission of contact form.

---
+++ application/controllers/Ajax.php
@@ -0,0 +1,5 @@
+<!---------------------
+Name: Michael Brady
+Intermediate Web Programming
+Purpose: This web application uses the CodeIgniter framework to display and create news headlines and articles.
+--------------------->  
---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+/*
+Name: Michael Brady
+Intermediate Web Programming
+Purpose: This web application uses the CodeIgniter framework to display and create news headlines and articles.
+*/
+
+"use strict";
