a480f73250062622c4594c7d9ddf7b6da60b3397
Michael Brady <michaelbrady712@gmail.com>
2023-05-14 10:20:34 -0500

    Added JS code to implement Google Maps API with additional functionality

--- assets/js/map.js
+++ assets/js/map.js
@@ -3,3 +3,44 @@ Name: Michael Brady
 Intermediate Web Programming
 Purpose: This web application uses the CodeIgniter framework to display and create news headlines and articles.
 */
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 40.7484, lng: -73.9857},
+    zoom: 8,
+  });
+
+  let markerOptions = {
+      position: new google.maps.LatLng(40.7484, -73.9857),
+      map: map,
+      label: "Headquarters",
+      title: "Our headquarters",
+      animation: google.maps.Animation.BOUNCE
+
+  }
+
+  let marker = new google.maps.Marker(markerOptions);
+
+  setTimeout(() => {
+      marker.setAnimation(null);
+  }, 5000);
+
+  const infoWindowOptions = {
+      content: "Producing high quality new reporting from the Empire State Building since 1975",
+  }
+
+  const infoWindow = new google.maps.InfoWindow(infoWindowOptions);
+
+  const infoWindowOpenOptions = {
+      map: map,
+      anchor: marker
+  }
+
+  infoWindow.open(infoWindowOpenOptions);
+
+  setTimeout(() => {
+      infoWindow.close();
+  }, 5000);
+
+}
